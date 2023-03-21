21f2f8fdd7bfc95393d7d45b43a50f7116b2267a
MohdBasurra <mohdye96@gmail.com>
2023-03-21 19:20:50 +0300

    assigment Completed

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Your code here
+// Your code here
+
+
+class Dog {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+    speak() {
+      return this.name + " says woof!";
+    }
+  }
+  
+class Cat {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+    speak() {
+      return this.name + " says meow!";
+    }
+  }
+  
+  
+
+  class Bird {
+
+   
+    constructor(name, sex) {
+      this.name = name;
+
+      this.sex = sex;
+
+      this.speak = function () {
+        if (this.sex === "male") {
+          return `It's me! ${this.name}, the parrot!`;
+        } else {
+          return `${this.name} says squawk!`;
+        }
+      };
+    }
+  }
+  
+
+  let Buzz = new Cat("Buzz", "male");
+
+  let Twitter = new Bird("Twitter", "male");
+  
+  console.log(Buzz.speak());
+
+
+  console.log(Twitter.speak());
